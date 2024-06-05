3feb123e2878c976f2e3a65670d65f04c1fb0512
adamk <adamkay123@live.com>
2024-06-04 22:53:54 -0700

    replaced duplicate code with function, fixed naming of variables

--- scraper.py
+++ scraper.py
@@ -10,68 +10,62 @@ treyarch_url = "https://careers.treyarch.com/search-results?keywords=vancouver"
 
 
 class Scraper:
-	def getJobsImperva(self, queue):
-		r = requests.get(imperva_url)
-		tree = html.fromstring(r.content)
-		li = tree.xpath('//li[contains(@class, "position-item") and @data-city="vancouver"]')
-		jobs = []
-		for posting in li:
+	def getHTML(self, url):
+		try:
+			response = requests.get(url)
+			response.raise_for_status()
+			return html.fromstring(response.content)
+		except requests.RequestException as e:
+			return
+
+	def extractJobs(self, postings, queue, company, titleXpath, linkXpath):
+		for posting in postings:
 			try:
-				title = posting.xpath(".//span[contains(@class, 'job-title')]/text()")[0]
-				link = posting.xpath("./a/@href")[0]
-				jobs.append(Job(title=title, link=link, company="Imperva"))
-			except IndexError:
+				title = posting.xpath(titleXpath)[0].strip()
+				link = posting.xpath(linkXpath)[0]
+				job = Job(title=title, link=link, company=company)
+				queue.put(job)
+			except IndexError as e:
 				continue
-		for job in jobs:
-			queue.put(job)
+
+	def getJobsImperva(self, queue):
+		tree = self.getHTML(imperva_url)
+		postings = tree.xpath('//li[contains(@class, "position-item") and @data-city="vancouver"]')
+		titleXPath = ".//span[contains(@class, 'job-title')]/text()"
+		linkXPath = "./a/@href"
+		self.extractJobs(postings, queue, "Imperva", titleXPath, linkXPath)
 
 	def getJobsEnea(self, queue):
-		r = requests.get(enea_url)
-		tree = html.fromstring(r.content)
-		spans = tree.xpath("//span[contains(text(), 'Canada') or contains(text(), 'Vancouver')]")
-		jobs = []
-		for span in spans:
-			try:
-				title = span.xpath("../preceding-sibling::span/text()")[0]
-				link = span.xpath("../../@href")[0]
-				company = "Enea"
-				jobs.append(Job(title=title, link=link, company=company))
-			except IndexError:
-				continue
-		for job in jobs:
-			queue.put(job)
+		tree = self.getHTML(enea_url)
+		postings = tree.xpath("//span[contains(text(), 'Canada') or contains(text(), 'Vancouver')]")
+		titleXPath = "../preceding-sibling::span/text()"
+		linkXPath = "../../@href"
+		self.extractJobs(postings, queue, "Enea", titleXPath, linkXPath)
 
 	def getJobsTreyarch(self, queue):
-		session = HTMLSession()
-		r = session.get(treyarch_url)
-		r.html.render()
-		html_content = r.html.html
-		parser = etree.HTMLParser()
-		tree = etree.fromstring(html_content, parser)
-		lis = tree.xpath("//li[contains(@class, 'jobs-list-item')]")
-		jobs = []
-		for li in lis:
-			try:
-				link = li.xpath(".//div[contains(@class, 'information')]/a/@href")[0]
-				title = li.xpath(".//div[contains(@class, 'job-title')]/span/text()")[0]
-				company = "Treyarch"
-				jobs.append(Job(title=title, link=link, company=company))
-			except IndexError:
-				continue
-		for job in jobs:
-			queue.put(job)
+		try:
+			session = HTMLSession()
+			r = session.get(treyarch_url)
+			r.html.render()
+			tree = etree.HTML(r.html.html)
+		except Exception as e:
+			return
+		postings = tree.xpath("//li[contains(@class, 'jobs-list-item')]")
+		titleXPath = ".//div[contains(@class, 'job-title')]/span/text()"
+		linkXPath = ".//div[contains(@class, 'information')]/a/@href"
+		self.extractJobs(postings, queue, "Treyarch", titleXPath, linkXPath)
 
 	def getAllJobs(self):
 		scrapers = [self.getJobsImperva, self.getJobsEnea, self.getJobsTreyarch]
-		processes = []
 		queue = Queue()
-		results = []
+		processes = []
 		for scraper in scrapers:
 			process = Process(target=scraper, args=(queue, ))
 			processes.append(process)
 			process.start()
 		for process in processes:
 			process.join()
+		results = []
 		while not queue.empty():
 			results.append(queue.get())
 		return results
