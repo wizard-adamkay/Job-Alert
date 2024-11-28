df87e06f29eaef75f8be4a98fc256e85721f8fbf
adamk <adamkay123@live.com>
2024-11-27 19:07:06 -0800

    Added scrapping for MDA:Space

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ from multiprocessing import Process, Queue
 imperva_url = "https://www.imperva.com/company/careers"
 enea_url = "https://careers.enea.com/jobs"
 treyarch_url = "https://careers.treyarch.com/search-results?keywords=vancouver"
-
+mda_url = "https://recruiting.ultipro.ca/MAC5000MCDW/JobBoard/664818ff-3594-4bec-9f30-3394e59e19f3/?q=&o=postedDateDesc&w=&wc=&we=&wpst=&f4=-NP_8IhlXF-fGXIRSM9fNw&f5=zbdts05UEUmmM9J30HpRfg+r6Nk37SKbUicJ3G-Ura_Tw"
 
 class Scraper:
 	def getHTML(self, url):
@@ -23,6 +23,8 @@ class Scraper:
 			try:
 				title = posting.xpath(titleXpath)[0].strip()
 				link = posting.xpath(linkXpath)[0]
+				if company == "MDA":
+					link = "https://recruiting.ultipro.ca/" + link
 				job = Job(title=title, link=link, company=company)
 				queue.put(job)
 			except IndexError as e:
@@ -55,8 +57,21 @@ class Scraper:
 		linkXPath = ".//div[contains(@class, 'information')]/a/@href"
 		self.extractJobs(postings, queue, "Treyarch", titleXPath, linkXPath)
 
+	def getJobsMDA(self, queue):
+		try:
+			session = HTMLSession()
+			r = session.get(mda_url)
+			r.html.render()
+			tree = etree.HTML(r.html.html)
+		except Exception as e:
+			return
+		postings = tree.xpath("//div[contains(@class, 'opportunity')]")
+		titleXPath = ".//h3/a[contains(@class, 'opportunity-link')]/text()"
+		linkXPath = ".//h3/a[contains(@class, 'opportunity-link')]/@href"
+		self.extractJobs(postings, queue, "MDA", titleXPath, linkXPath)
+
 	def getAllJobs(self):
-		scrapers = [self.getJobsImperva, self.getJobsEnea, self.getJobsTreyarch]
+		scrapers = [self.getJobsImperva, self.getJobsEnea, self.getJobsTreyarch, self.getJobsMDA]
 		queue = Queue()
 		processes = []
 		for scraper in scrapers:
